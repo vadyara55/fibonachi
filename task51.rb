137ddd9dc945f7a47a78a5e3a4a743ca0d2e3bec
vadyara55 <vadyara55@gmail.com>
2019-02-19 22:54:47 +0600

    change

--- task51.rb
+++ task51.rb
@@ -1,13 +1,14 @@
-print "Введите день"
+print "Введите день: "
 day = gets.to_i
-print "Введите месяц"
+print "Введите месяц: "
 month = gets.to_i
-print "Введите год"
+print "Введите год: "
 year = gets.to_i
 months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 #определение високосного года
-months[1] = 29 if year % 4 == 0 && year % 100 != 0 or year % 400 == 0
-  sum = 0
-  months[0..month-2].each { |mn| sum +=mn  }
+sum = 0
+leap_year= year % 4 == 0 && year % 100 != 0 ||year % 400 == 0
+months[1] = 29 if leap_year
+  months[0..month-2].each { |mn| sum +=mn }
 sum += day
-puts sum
+puts "Порядковый номер дня: #{sum}"
