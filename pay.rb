5cf97449ad807fe75a5d5d29b225855bfa7b78bd
vadyara55 <vadyara55@gmail.com>
2019-02-20 23:51:42 +0600

    change

--- pay.rb
+++ pay.rb
@@ -1,18 +1,17 @@
 cart = {}
 sum = 0
 loop do
-  print "Введите название товара или (\"cтоп\"): "
+  print "Введите название товара или (\"стоп\"): "
   name = gets.chomp
-  break if name == "cтоп"
+  break if name == "стоп"
   print "Цену: "
   price = gets.chomp.to_f
   print "Количество: "
   count = gets.chomp.to_f
 
   cart[name] = { "price" => price, "count" => count }
+  puts cart
 end
 
-cart.each {|name, hash| sum += hash["price"] * hash["count"]}
-
-puts cart
+cart.each {|names, hash| sum += hash["price"] * hash["count"]}
 puts "Сумма покупок #{sum}"
